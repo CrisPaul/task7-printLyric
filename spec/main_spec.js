dc809fb697fa71400b12ccdcc7d32d7a13aa5b77
CrisPaul <30457366+CrisPaul@users.noreply.github.com>
2017-08-11 19:44:51 +0800

    First time commit for task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,22 +10,59 @@ var main = require("../lib/main.js");
 
 
 describe("测试描述", function(){
-    sinon.spy(console, 'log');
+    //sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("Return a lyric when input 0", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(0);
+        var expect_string = 'No more bottles of beer on the wall, no more bottles of beer.\n' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.\n';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("Return a lyric when input 1", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var result = main(1);
+        var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+            'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+            'No more bottles of beer on the wall, no more bottles of beer.\n' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.\n';
 
         expect(expect_string).to.equal(result);
     });
+
+    it("Return a lyric when input 2", function(){
+
+        var result = main(2);
+        var expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+            'Take one down and pass it around, 1 bottle of beer on the wall.\n' +
+            '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+            'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+            'No more bottles of beer on the wall, no more bottles of beer.\n' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.\n';
+
+        expect(expect_string).to.equal(result);
+    });
+
+
+    it("Return a lyric when input 15", function(){
+
+        var result = main(5);
+        var expect_string = '5 bottles of beer on the wall, 5 bottles of beer.\n' +
+            'Take one down and pass it around, 4 bottles of beer on the wall.\n' +
+            '4 bottles of beer on the wall, 4 bottles of beer.\n' +
+            'Take one down and pass it around, 3 bottles of beer on the wall.\n' +
+            '3 bottles of beer on the wall, 3 bottles of beer.\n' +
+            'Take one down and pass it around, 2 bottles of beer on the wall.\n' +
+            '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+            'Take one down and pass it around, 1 bottle of beer on the wall.\n' +
+            '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+            'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+            'No more bottles of beer on the wall, no more bottles of beer.\n' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.\n';
+
+        expect(expect_string).to.equal(result);
+    });
+
 });
